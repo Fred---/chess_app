199c1bdc7b1d681e36810f86164bfc7d744bdba8
Fred <labelle.frederic@gmail.com>
2014-04-27 15:56:19 +0200

    Heroku setup...

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -39,18 +39,18 @@ describe "Static pages" do
     end
   end
 
-  describe "Help page" do
-    before { visit help_path }
-
-    it { should have_content('Help') }
-    it { should have_title(full_title('Help')) }
-  end
-
   describe "About page" do
     before { visit about_path }
 
     it { should have_content('About') }
     it { should have_title(full_title('About Us')) }
+  end
+
+  describe "Chess page" do
+    before { visit chess_path }
+
+    it { should have_content('Chess') }
+    it { should have_title(full_title('Chess Playground')) }
   end
 
   describe "Contact page" do
